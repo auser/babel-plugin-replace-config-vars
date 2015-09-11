77e84e04f23c1fc4c9a2deef0326de98636ba861
Ari Lerner <arilerner@mac.com>
2015-09-11 13:11:25 -0700

    Updated with debugging info

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 var fs = require('fs'),
     path = require('path');
 
+const DEBUG_INTRO = 'Babel plugin config: ';
 var env = process.env.NODE_ENV || 'development';
+const defaultConfig = {
+  "directory": "./config"
+};
 
 export default function ({ Plugin, types: t }) {
   var match = t.buildMatchMemberExpression("process.env");
+  var configOpts       = (opts) => (opts.extra && opts.extra.config) ? opts.extra.config : defaultConfig;
   var directoryDefined = (opts) => opts.extra && opts.extra.config && opts.extra.config.directory;
   var defaultDirectory = './config';
 
   return new Plugin("replace-config-vars", {
     visitor: {
       Program(node, parent, scope, file) {
-        // console.log('node ->', file);
+        // Set debugging
+        let debugging = configOpts.debug ? configOpts.debug : false;
+        file.set("debugging", debugging);
+
         var dir = directoryDefined(file.opts) ?
             file.opts.extra.config.directory : defaultDirectory;
 
@@ -20,60 +28,39 @@ export default function ({ Plugin, types: t }) {
         var conf = {};
 
         // Check if file exists
+
+        debugging && console.log(DEBUG_INTRO, "Attempting to load config file: " + confFile);
+
         try {
           var stat = fs.statSync(confFile);
-          if (stat.isFile())
+          if (stat.isFile()) {
             conf = require(confFile);
+            debugging && console.log(DEBUG_INTRO, "Loaded config file:", confFile);
+          }
         } catch (e) {
           if (e.code !== 'ENOENT')
             throw new Error(e);
+
+          debugging && console.log(DEBUG_INTRO, "Error loading config file",e);
         }
 
         file.set("config", conf);
       },
 
       MemberExpression: function(node, parent, scope, state) {
+        let debugging = state.get('debugging');
+
         if (match(node.object)) {
           var cfg = state.get('config');
           var key = this.toComputedKey();
           if (cfg[key.value]) {
-          var value = cfg[key.value];
+            var value = cfg[key.value];
+            
+            debugging && console.log(DEBUG_INTRO, "Setting ", key, "to", value);
             return t.valueToNode(value);
           }
         }
       }
-
-      // Scopable(node, parent, scope, file) {
-      //   for (var name in this.scope.bindings) {
-      //     console.log('name --->', this.scope.globals);
-      //     // this.scope.rename(name, toEmoji(name, file.get("emojificationMap")));
-      //   }
-      // },
-      // MemberExpression(node, parent, scope, state) {
-      //   var prop = node.property;
-      //   if (t.isIdentifier(node.object) && node.object.name === 'process') {
-      //     // if (node.computed && )
-      //     var isMemberExpression = t.isMemberExpression({computed: false});
-      //     var callee = this.get('callee');
-
-      //     console.log('env ->',
-      //                 isMemberExpression,
-      //                 callee,
-      //                 this.traverse(node.property)
-      //                 // t.toExpression(node),
-      //                 // t.memberExpression(node)
-      //                );
-
-      //     //   if (node.property.name === 'env') {
-      //     //     var property = node.property;
-      //     //     console.log('replace ->', property, property.name);
-      //     //   }
-      //     //     var property = node.property;
-      //     //     var defines = state.opts.extra.defines;
-      //     //     if (t.isIdentifier(property) && defines[property.name] !== undefined) {
-      //     //       return t.valueToNode(state.opts.extra.defines[property.name]);
-      //   }
-      // }
-    }
+   }
   });
 }
