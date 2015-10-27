07f84419447838b3a9e57ac10e025a680638c1be
Ari Lerner <arilerner@mac.com>
2015-10-26 23:14:42 -0700

    Updated linted file

--- src/index.js
+++ src/index.js
@@ -1,50 +1,52 @@
+/*eslint-disable no-process-env, no-undef*/
+
 import fs from 'fs';
 import path from 'path';
-import glob from 'glob';
-import objectAssign from 'object-assign';
 
-var env = process.env.NODE_ENV || 'development';
-var defaultConfig = [];
+let env = process.env.NODE_ENV || 'development';
+let defaultConfig = [];
 
 export default function ({ Plugin, types: t }) {
-  var match = t.buildMatchMemberExpression('process.env');
-  var configOpts       = (opts) => {
+  let match = t.buildMatchMemberExpression('process.env');
+  let configOpts       = (opts) => {
     let extraCfg  = (opts.extra && opts.extra.config) ? opts.extra.config : {};
     return defaultConfig.concat(extraCfg);
-  }
+  };
+
+  const requireConfig = (filepath) => require(filepath);
 
   const buildStringConfig = (filePath) => {
     try {
       let stat = fs.statSync(filePath);
       if (stat.isFile()) {
-        return require(path.resolve(filePath));
+        return requireConfig(path.resolve(filePath));
       }
     } catch (e) {
-      console.log('Error occurred when opening file', e);
+      return {};
     }
     return {};
-  }
+  };
 
   const buildConfig = (opts) => {
     let confOpts = configOpts(opts);
     return confOpts.reduce((memo, item) => {
       let cfg = memo;
       switch(typeof item) {
-        case 'string':
-          let newStr = item.replace(/\$ENV/, env);
-          Object.assign(cfg, buildStringConfig(newStr));
-          break;
-        case 'object':
-          if (item[env]) {
-            Object.assign(cfg, item[env]);
-          }
-          break;
-        default:
-          console.error('Unknown config type');
+      case 'string':
+        let newStr = item.replace(/\$ENV/, env);
+        Object.assign(cfg, buildStringConfig(newStr));
+        break;
+      case 'object':
+        if (item[env]) {
+          Object.assign(cfg, item[env]);
+        }
+        break;
+      default:
+        break;
       }
       return cfg;
     }, {});
-  }
+  };
 
   return new Plugin('replace-config-vars', {
     visitor: {
@@ -56,11 +58,15 @@ export default function ({ Plugin, types: t }) {
 
       MemberExpression: function(node, parent, scope, state) {
         if (match(node.object)) {
-          var cfg = state.get('config');
-          var key = this.toComputedKey();
+          let processEnv;
+          if (typeof process !== 'undefined' && typeof process.env !== 'undefined') {
+            processEnv = process.env;
+          }
+          let cfg = state.get('config');
+          let key = this.toComputedKey();
           let k = key.value.toUpperCase();
           if (cfg[k] || process.env[k]) {
-            var value = cfg[k] || process.env[k];
+            let value = cfg[k] || processEnv[k];
             return t.valueToNode(value);
           }
         }
